8a837a6f78b2cd44690e73de1e584ff9d16edb3d
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-07-24 14:23:52 +0700

    Update rest_API.sh

--- rest_API.sh
+++ rest_API.sh
@@ -21,7 +21,7 @@
 #     -H "X-CSRFToken: $csrf_token" \
 #     -H "Authorization: Bearer $bearer" \
 #     -H "Cookie: $cookie" >export.zip
-curl --location 'https://superset-sit.dev.kea.ifdsfs.com/api/v1/status' \
+curl --location 'https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login' \
             --header 'Content-Type: application/json' \
             --data-raw '{
               "password": "MoOk-484521",
