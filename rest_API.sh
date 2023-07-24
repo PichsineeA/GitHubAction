0f063f85f14942a052beaa54ed90350dbb4e1529
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-07-24 14:34:50 +0700

    Update rest_API.sh

--- rest_API.sh
+++ rest_API.sh
@@ -21,11 +21,12 @@
 #     -H "X-CSRFToken: $csrf_token" \
 #     -H "Authorization: Bearer $bearer" \
 #     -H "Cookie: $cookie" >export.zip
-curl --location 'https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login' \
-            --header 'Content-Type: application/json' \
-            --data-raw '{
-              "password": "MoOk-484521",
-              "provider": "db",
-              "refresh": true,
-              "username": "pichsinee.angsuchaikij@sscinc.com"
-            }'
+# curl --location 'https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login' \
+#             --header 'Content-Type: application/json' \
+#             --data-raw '{
+#               "password": "MoOk-484521",
+#               "provider": "db",
+#               "refresh": true,
+#               "username": "pichsinee.angsuchaikij@sscinc.com"
+#             }'
+curl --location 'https://simple-books-api.glitch.me/status'
