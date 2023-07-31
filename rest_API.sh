f8d62adcb5ecf35de6db1f79fa21b51f75979340
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-07-31 15:46:13 +0700

    Update rest_API.sh

--- rest_API.sh
+++ rest_API.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-cookie=$(curl -sSL -D - https://superset-sit.dev.kea.ifdsfs.com/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+# cookie=$(curl -sSL -D - https://superset-sit.dev.kea.ifdsfs.com/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login --data '{ "username": "Pichsinee.angsuchaikij@sscinc.com", "password": "MoOk-484521", "provider": "db"}' | jq -r '.access_token')
+# bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login --data '{ "username": "Pichsinee.angsuchaikij@sscinc.com", "password": "MoOk-484521", "provider": "db"}' | jq -r '.access_token')
 
-csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/csrf_token/ | jq -r '.result')
+# csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/csrf_token/ | jq -r '.result')
 
-echo ${cookie}
-echo "================================================================================"
-echo ${bearer}
-echo "================================================================================"
-echo ${csrf_token}
-echo "================================================================================"
+# echo ${cookie}
+# echo "================================================================================"
+# echo ${bearer}
+# echo "================================================================================"
+# echo ${csrf_token}
+# echo "================================================================================"
 
-curl -X 'GET' \
-    'https://superset-sit.dev.kea.ifdsfs.com/api/v1/dashboard/export/?q=!(7%2C8%2C9)' \
-    -H 'accept: */*' \
-    -H 'Accept-Encoding: gzip, deflate, br' \
-    -H 'Content-Type: multipart/form-data' \
-    -H "X-CSRFToken: $csrf_token" \
-    -H "Authorization: Bearer $bearer" \
-    -H "Cookie: $cookie" >export.zip
+# curl -X 'GET' \
+#     'https://superset-sit.dev.kea.ifdsfs.com/api/v1/dashboard/export/?q=!(7%2C8%2C9)' \
+#     -H 'accept: */*' \
+#     -H 'Accept-Encoding: gzip, deflate, br' \
+#     -H 'Content-Type: multipart/form-data' \
+#     -H "X-CSRFToken: $csrf_token" \
+#     -H "Authorization: Bearer $bearer" \
+#     -H "Cookie: $cookie" >export.zip
 # curl -X 'POST' \
 #             'https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login' \
 #             --header 'Content-Type: application/json' \
@@ -38,3 +38,4 @@ curl -X 'GET' \
 #    "clientName": "Postman",
 #    "clientEmail": "pichs@example.com"
 # }'
+curl 'https://superset-sit.dev.kea.ifdsfs.com/api/v1/example/greeting2'
