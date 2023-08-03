064b2332c6229d393d213e5c18b891d92e0625bf
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-03 09:24:23 +0700

    Update rest_API.sh

--- rest_API.sh
+++ rest_API.sh
@@ -38,4 +38,29 @@
 #    "clientName": "Postman",
 #    "clientEmail": "pichs@example.com"
 # }'
-curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login --data '{ "username": "Pichsinee.angsuchaikij@sscinc.com", "password": "MoOk-484521", "provider": "ldap", "refresh": true}'
+# curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  https://superset-sit.dev.kea.ifdsfs.com/api/v1/security/login --data '{ "username": "Pichsinee.angsuchaikij@sscinc.com", "password": "MoOk-484521", "provider": "ldap", "refresh": true}'
+#!/bin/bash
+clear
+cookie=$(curl -sSL -D - http://localhost:8088/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  http://localhost:8088/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
+
+csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" http://localhost:8088/api/v1/security/csrf_token/ | jq -r '.result')
+
+echo ${cookie}
+echo "================================================================================"
+echo ${bearer}
+echo "================================================================================"
+echo ${csrf_token}
+echo "================================================================================"
+
+curl -X 'GET' \
+    'http://localhost:8088/api/v1/dashboard/export/?q=!(7%2C8%2C9)' \
+    -H 'accept: */*' \
+    -H 'Accept-Encoding: gzip, deflate, br' \
+    -H 'Content-Type: multipart/form-data' \
+    -H "X-CSRFToken: $csrf_token" \
+    -H "Authorization: Bearer $bearer" \
+    -H "Cookie: $cookie" >export.zip
+#unzip
+unzip export.zip
