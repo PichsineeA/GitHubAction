86046dc9c7e4b3702a5a9d97644d7dc08d408d8a
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-03 10:02:03 +0700

    Delete superset_setup.sh

--- superset_setup.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/apache/superset.git
-cd superset
-git checkout 2.0.0
-TAG=2.0.0 docker-compose -f docker-compose-non-dev.yml pull
-TAG=2.0.0 docker-compose -f docker-compose-non-dev.yml up
