1743b2e6378e5b9b9747379926bfbcd6573c78fb
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-03 09:50:59 +0700

    Create import_local.sh

---
+++ import_local.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cookie=$(curl -sSL -D - http://localhost:8088/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  http://localhost:8088/api/v1/security/login --data '{ "username": "admin", "password": "123456", "provider": "db"}' | jq -r '.access_token')
+
+csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" http://localhost:8088/api/v1/security/csrf_token/ | jq -r '.result')
+
+echo ${cookie}
+echo "================================================================================"
+echo ${bearer}
+echo "================================================================================"
+echo ${csrf_token}
+echo "================================================================================"
+dashboard=dashboard_export_20230630T021218.zip
+
+curl -X 'POST' \
+    'http://localhost:8088/api/v1/dashboard/import/' \
+    -H 'accept: application/json' \
+    -H 'Content-Type: multipart/form-data' \
+    -H "X-CSRFToken: $csrf_token" \
+    -H "Authorization: Bearer $bearer" \
+    -H 'Origin: http://localhost:8088' \
+    -H "Cookie: $cookie" \
+    -F "formData=@${dashboard}" \
+    -F "overwrite=true"
