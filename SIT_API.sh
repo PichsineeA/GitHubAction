bc7f71f7b363656991cc25f3b657b6880253f730
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-04 09:35:01 +0700

    Update SIT_API.sh

--- SIT_API.sh
+++ SIT_API.sh
@@ -14,7 +14,7 @@ echo ${csrf_token}
 echo "================================================================================"
 
 curl -X 'GET' \
-    'http://localhost:8088/api/v1/assets/export/' \
+    'https://superset.dev.kea.ifdsfs.com/api/v1/assets/export/' \
     -H 'accept: */*' \
     -H 'Accept-Encoding: gzip, deflate, br' \
     -H 'Content-Type: multipart/form-data' \
