52dbc50b0c43806845ee839a94db8f7dc1bd7645
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-04 12:05:14 +0700

    Update SIT_API.sh

--- SIT_API.sh
+++ SIT_API.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cookie=$(curl -sSL -D - https://superset-sit.dev.kea.ifdsfs.com/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+# cookie=$(curl -sSL -D - https://superset-sit.dev.kea.ifdsfs.com/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  https://superset.dev.kea.ifdsfs.com/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
+# bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  https://superset.dev.kea.ifdsfs.com/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
 
-csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" https://superset.dev.kea.ifdsfs.com/api/v1/security/csrf_token/ | jq -r '.result')
+# csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" https://superset.dev.kea.ifdsfs.com/api/v1/security/csrf_token/ | jq -r '.result')
 
-echo ${cookie}
-echo "================================================================================"
-echo ${bearer}
-echo "================================================================================"
-echo ${csrf_token}
-echo "================================================================================"
-curl -X 'GET' \
-  'https://superset.dev.kea.ifdsfs.com/api/v1/assets/export/' \
-  -H 'accept: application/zip' \
-  -H 'Authorization: Bearer $bearer'
+# echo ${cookie}
+# echo "================================================================================"
+# echo ${bearer}
+# echo "================================================================================"
+# echo ${csrf_token}
+# echo "================================================================================"
+# curl -X 'GET' \
+#   'https://superset.dev.kea.ifdsfs.com/api/v1/assets/export/' \
+#   -H 'accept: application/zip' \
+#   -H 'Authorization: Bearer $bearer'
 # curl -X 'GET' \
 #     'https://superset.dev.kea.ifdsfs.com/api/v1/assets/export/' \
 #     -H 'accept: */*' \
@@ -26,3 +26,12 @@ curl -X 'GET' \
 #     -H "Cookie: $cookie" >assets.zip
 #unzip
 #unzip export.zip
+
+curl --location 'https://superset.dev.kea.ifdsfs.com/api/v1/security/login' \
+--header 'Content-Type: application/json' \
+--data '{
+  "password": "admin",
+  "provider": "db",
+  "refresh": true,
+  "username": "admin"
+}'
