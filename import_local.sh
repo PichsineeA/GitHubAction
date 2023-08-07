6b7aa2ffa9c3e5004b14ad2420fdfea5306a60ac
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-08 00:26:51 +0700

    Update import_local.sh

--- import_local.sh
+++ import_local.sh
@@ -12,7 +12,7 @@ echo ${bearer}
 echo "================================================================================"
 echo ${csrf_token}
 echo "================================================================================"
-dashboard=exportDataset.zip #dashboard_export_20230630T021218.zip
+dashboard=exportDatasets.zip #dashboard_export_20230630T021218.zip
 
 curl -X 'POST' \
     'http://localhost:8088/api/v1/dataset/import/' \
