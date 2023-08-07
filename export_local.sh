104dbd3b3a57db6525d519ba5a00f4737a0ea33f
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-07 23:27:10 +0700

    Update export_local.sh

--- export_local.sh
+++ export_local.sh
@@ -14,12 +14,12 @@ echo ${csrf_token}
 echo "================================================================================"
 
 curl -X 'GET' \
-    'http://localhost:8088/api/v1/dashboard/export/?q=!(7%2C8%2C9)' \
+    'http://localhost:8088/api/v1/assets/export/' \
     -H 'accept: */*' \
     -H 'Accept-Encoding: gzip, deflate, br' \
     -H 'Content-Type: multipart/form-data' \
     -H "X-CSRFToken: $csrf_token" \
     -H "Authorization: Bearer $bearer" \
-    -H "Cookie: $cookie">export.zip
+    -H "Cookie: $cookie">export_all.zip
 #unzip
 # unzip export.zip
