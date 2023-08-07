70cbe39f64c7a30c7cb29b1c9b705734b5200740
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-07 23:45:04 +0700

    Update export_local.sh

--- export_local.sh
+++ export_local.sh
@@ -14,12 +14,12 @@ echo ${csrf_token}
 echo "================================================================================"
 
 curl -X 'GET' \
-    'http://localhost:8088/api/v1/chart/export/?q=!(71)' \
+    'http://localhost:8088/api/v1/chart/export/?q=!(71,113)' \
     -H 'accept: */*' \
     -H 'Accept-Encoding: gzip, deflate, br' \
     -H 'Content-Type: multipart/form-data' \
     -H "X-CSRFToken: $csrf_token" \
     -H "Authorization: Bearer $bearer" \
-    -H "Cookie: $cookie" >exportChart.zip
+    -H "Cookie: $cookie" >exportCharts.zip
 #unzip
 # unzip export.zip
