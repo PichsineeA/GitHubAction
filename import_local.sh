82f7fe3abe1d05b29b71e3f84056a6251c1c7d28
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-08 09:10:43 +0700

    Update import_local.sh

--- import_local.sh
+++ import_local.sh
@@ -14,13 +14,21 @@ echo ${csrf_token}
 echo "================================================================================"
 dashboard=exportDatabase.zip #dashboard_export_20230630T021218.zip
 
-curl -X 'POST' \
-    'http://localhost:8088/api/v1/database/import/' \
-    -H 'accept: application/json' \
-    -H 'Content-Type: multipart/form-data' \
-    -H "X-CSRFToken: $csrf_token" \
-    -H "Authorization: Bearer $bearer" \
-    -H 'Origin: http://localhost:8088' \
-    -H "Cookie: $cookie" \
-    -F "formData=@${dashboard}" \
-    -F "overwrite=true"
+# curl -X 'POST' \
+#     'http://localhost:8088/api/v1/database/import/' \
+#     -H 'accept: application/json' \
+#     -H 'Content-Type: multipart/form-data' \
+#     -H "X-CSRFToken: $csrf_token" \
+#     -H "Authorization: Bearer $bearer" \
+#     -H 'Origin: http://localhost:8088' \
+#     -H "Cookie: $cookie" \
+#     -F "formData=@${dashboard}" \
+#     -F "overwrite=true"
+
+curl --location 'http://localhost:8088/api/v1/database/import/' \
+--header 'X-CSRFToken: ImI1NDgwY2JkNTllYThkOWFmZTlhN2UwZjBkMjViMzFiYzlmODE5NDAi.ZNGizg.U8Xo91mvw76VvIv6W4U-dYYBbMo' \
+--header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6dHJ1ZSwiaWF0IjoxNjkxNDYwMjg3LCJqdGkiOiI3ZDU4NDMyYy0wZGQzLTQ3NTItYmE3ZC05ZmFkZjBlZTM3ZmYiLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoxLCJuYmYiOjE2OTE0NjAyODcsImV4cCI6MTY5MTQ2MTE4N30.lnQrtN1_dotnMdWSayMftd5m-9RYQ7Yz1Nr9C7Fl4GY' \
+--header 'Cookie: session=eyJfZnJlc2giOmZhbHNlLCJjc3JmX3Rva2VuIjoiYjU0ODBjYmQ1OWVhOGQ5YWZlOWE3ZTBmMGQyNWIzMWJjOWY4MTk0MCJ9.ZNGj4Q.djJo8T-9Ep1BWV5J1ZfdDBM5X0s' \
+--form 'formData=@"exportDatabase.zip"' \
+--form 'overwrite="true"' \
+--form 'passwords="{}"'
