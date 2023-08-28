a89146ebaaba231b67fc59a57e93fb5b0e3c18e9
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-28 21:11:15 +0700

    Update backup.sh

--- pipeline_script_github/backup.sh
+++ pipeline_script_github/backup.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-cookie=$(curl -sSL -D - ${ENV_HOST}/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+echo '${ENV_HOST}/api/v1/assets/export/'
+echo ${ENV_USER}
+echo ${ENV_PASS}
+# ${ENV_NAME}
+# ${ENV_HOST}
+# ${ENV_PASS}
+# ${ENV_USER}
 
-bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "${ENV_USER}", "password": "${ENV_PASS}", "provider": "db"}' | jq -r '.access_token')
+# cookie=$(curl -sSL -D - ${ENV_HOST}/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" ${ENV_HOST}/api/v1/security/csrf_token/ | jq -r '.result')
+# bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "${ENV_USER}", "password": "${ENV_PASS}", "provider": "db"}' | jq -r '.access_token')
 
-echo ${cookie}
-echo "================================================================================"
-echo ${bearer}
-echo "================================================================================"
-echo ${csrf_token}
+# csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" ${ENV_HOST}/api/v1/security/csrf_token/ | jq -r '.result')
 
-curl -X 'GET' \
-    '${ENV_HOST}/api/v1/assets/export/' \
-    -H 'accept: */*' \
-    -H 'Accept-Encoding: gzip, deflate, br' \
-    -H 'Content-Type: multipart/form-data' \
-    -H "X-CSRFToken: $csrf_token" \
-    -H "Authorization: Bearer $bearer" \
-    -H "Cookie: $cookie" >${ENV_NAME}_backup_assets.zip
+# echo ${cookie}
+# echo "================================================================================"
+# echo ${bearer}
+# echo "================================================================================"
+# echo ${csrf_token}
+
+# curl -X 'GET' \
+#     '${ENV_HOST}/api/v1/assets/export/' \
+#     -H 'accept: */*' \
+#     -H 'Accept-Encoding: gzip, deflate, br' \
+#     -H 'Content-Type: multipart/form-data' \
+#     -H "X-CSRFToken: $csrf_token" \
+#     -H "Authorization: Bearer $bearer" \
+#     -H "Cookie: $cookie" >${ENV_NAME}_backup_assets.zip
 #unzip
 # unzip export.zip
