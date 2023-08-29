cffc82607fd2b4fcbe3fa58cf60ee37e443cf9b9
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-29 09:29:30 +0700

    Update backup.sh

--- pipeline_script_github/backup.sh
+++ pipeline_script_github/backup.sh
@@ -7,7 +7,7 @@
 
 cookie=$(curl -sSL -D - ${ENV_HOST}/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "${ENV_USER}", "password": "${ENV_PASS}", "provider": "db"}' | jq -r '.access_token')
 
 csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" ${ENV_HOST}/api/v1/security/csrf_token/ | jq -r '.result')
 
