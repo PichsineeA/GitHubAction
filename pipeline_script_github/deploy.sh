b2d39a20ea0b937f4fab4c41eac5024ab06d5153
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-29 09:39:06 +0700

    Update deploy.sh

--- pipeline_script_github/deploy.sh
+++ pipeline_script_github/deploy.sh
@@ -2,7 +2,7 @@
 
 cookie=$(curl -sSL -D - ${ENV_HOST}/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "${ENV_USER}", "password": "${ENV_PASS}", "provider": "db"}' | jq -r '.access_token')
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "'${ENV_USER}'", "password": "'${ENV_PASS}'", "provider": "db"}' | jq -r '.access_token')
 
 csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" ${ENV_HOST}/api/v1/security/csrf_token/ | jq -r '.result')
 
@@ -12,16 +12,16 @@ echo ${bearer}
 echo "================================================================================"
 echo ${csrf_token}
 
-dashboard=exportDatabaseDeleteDS.zip #dashboard_export_20230630T021218.zip
+#dashboard=exportDatabaseDeleteDS.zip #dashboard_export_20230630T021218.zip
 
 curl -X 'POST' \
-    '${ENV_HOST}/api/v1/database/import/' \
+    ''${ENV_HOST}'/api/v1/assets/import/' \
     -H 'accept: application/json' \
     -H 'Content-Type: multipart/form-data' \
     -H "X-CSRFToken: $csrf_token" \
     -H "Authorization: Bearer $bearer" \
-    -H 'Origin: http://localhost:8088' \
+    -H 'Origin: '${ENV_HOST}'' \
     -H "Cookie: $cookie" \
-    -F "formData=@${dashboard}" \
+    -F "formData=@${ASSETS}" \
     -F "overwrite=true" \
     -F "passwords="{}""
