5913c32492fdae117699c0d332b21d908137e2a7
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-30 09:49:25 +0700

    Update rollback.sh

--- pipeline_script_github/rollback.sh
+++ pipeline_script_github/rollback.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 pwd
-cd pipeline_script_github/backup
-pwd
-ls
-# cookie=$(curl -sSL -D - http://localhost:8088/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+cd ${ARTIFACT_BACKUP_PATH}
 
-# bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  http://localhost:8088/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
+cookie=$(curl -sSL -D - http://localhost:8088/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-# csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" http://localhost:8088/api/v1/security/csrf_token/ | jq -r '.result')
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  http://localhost:8088/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
 
-# echo ${cookie}
-# echo "================================================================================"
-# echo ${bearer}
-# echo "================================================================================"
-# echo ${csrf_token}
-# echo "================================================================================"
-# dashboard=assets_export_20230829T045823.zip
+csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" http://localhost:8088/api/v1/security/csrf_token/ | jq -r '.result')
 
-# curl -X 'POST' \
-#     'http://localhost:8088/api/v1/assets/import/' \
-#     -H 'accept: application/json' \
-#     -H 'Content-Type: multipart/form-data' \
-#     -H "X-CSRFToken: $csrf_token" \
-#     -H "Authorization: Bearer $bearer" \
-#     -H 'Origin: http://localhost:8088' \
-#     -H "Cookie: $cookie" \
-#     -F "bundle=@${dashboard}" \
-#     -F "overwrite=true"
+echo ${cookie}
+echo "================================================================================"
+echo ${bearer}
+echo "================================================================================"
+echo ${csrf_token}
+echo "================================================================================"
+
+curl -X 'POST' \
+    'http://localhost:8088/api/v1/assets/import/' \
+    -H 'accept: application/json' \
+    -H 'Content-Type: multipart/form-data' \
+    -H "X-CSRFToken: $csrf_token" \
+    -H "Authorization: Bearer $bearer" \
+    -H 'Origin: http://localhost:8088' \
+    -H "Cookie: $cookie" \
+    -F "bundle=@${ARTIFACT_BACKUP_File}" \
+    -F "overwrite=true"
