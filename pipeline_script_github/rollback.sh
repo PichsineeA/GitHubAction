24af7be303e446a5d152127dfe81d98d20e8a363
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-08-30 15:33:33 +0700

    Update rollback.sh

--- pipeline_script_github/rollback.sh
+++ pipeline_script_github/rollback.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cookie=$(curl -sSL -D - http://localhost:8088/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+cookie=$(curl -sSL -D - ${ENV_HOST}/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
 
-bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  http://localhost:8088/api/v1/security/login --data '{ "username": "admin", "password": "admin", "provider": "db"}' | jq -r '.access_token')
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "'${ENV_USER}'", "password": "'${ENV_PASS}'", "provider": "db"}' | jq -r '.access_token')
 
-csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" http://localhost:8088/api/v1/security/csrf_token/ | jq -r '.result')
+csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" ${ENV_HOST}/api/v1/security/csrf_token/ | jq -r '.result')
 
 echo ${cookie}
 echo "================================================================================"
@@ -12,14 +12,15 @@ echo ${bearer}
 echo "================================================================================"
 echo ${csrf_token}
 echo "================================================================================"
+ASSETS=assets_export_20230829T045823.zip
 
 curl -X 'POST' \
-    'http://localhost:8088/api/v1/assets/import/' \
+    ''${ENV_HOST}'/api/v1/assets/import/' \
     -H 'accept: application/json' \
     -H 'Content-Type: multipart/form-data' \
     -H "X-CSRFToken: $csrf_token" \
     -H "Authorization: Bearer $bearer" \
-    -H 'Origin: http://localhost:8088' \
+    -H 'Origin: '${ENV_HOST}'' \
     -H "Cookie: $cookie" \
-    -F "bundle=@${ARTIFACT_BACKUP_File}" \
+    -F "bundle=@${ASSETS}" \
     -F "overwrite=true"
