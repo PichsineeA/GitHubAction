04b76ce9545687d02b3d93fbd1ad5ba6df8def2b
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-09-07 14:01:27 +0700

    Create getAssets.sh

---
+++ pipeline_script_github/getAssets.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cookie=$(curl -sSL -D - ${ENV_HOST}/login -o /dev/null | grep Set-Cookie | cut -d : -f 2 | sed 's/ //')
+
+bearer=$(curl --header 'Content-Type: application/json' --header "Cookie: $cookie" -XPOST  ${ENV_HOST}/api/v1/security/login --data '{ "username": "'${ENV_USER}'", "password": "'${ENV_PASS}'", "provider": "db"}' | jq -r '.access_token')
+
+csrf_token=$(curl --header "Authorization: Bearer $bearer" --header "Cookie: $cookie" ${ENV_HOST}/api/v1/security/csrf_token/ | jq -r '.result')
+
+echo ${cookie}
+echo "================================================================================"
+echo ${bearer}
+echo "================================================================================"
+echo ${csrf_token}
+
+curl -X 'GET' \
+    ''${ENV_HOST}'/api/v1/assets/export/' \
+    -H 'accept: */*' \
+    -H 'Accept-Encoding: gzip, deflate, br' \
+    -H 'Content-Type: multipart/form-data' \
+    -H "X-CSRFToken: $csrf_token" \
+    -H "Authorization: Bearer $bearer" \
+    -H "Cookie: $cookie" >pipeline_script_github/backup/${ENV_NAME}_get_assets.zip
