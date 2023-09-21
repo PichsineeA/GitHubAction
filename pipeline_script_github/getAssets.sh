3530861e16a19f178666a26742100aca3fff3a40
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-09-21 10:40:11 +0700

    Update getAssets.sh

--- pipeline_script_github/getAssets.sh
+++ pipeline_script_github/getAssets.sh
@@ -21,10 +21,12 @@ curl -X 'GET' \
     -H "Authorization: Bearer $bearer" \
     -H "Cookie: $cookie" >pipeline_script_github/backup/${ENV_NAME}_get_assets.zip
 
-#remove old version assets
-rm -r Superset_assets
-
 # unzip file
 unzip "pipeline_script_github/backup/${ENV_NAME}_get_assets.zip"
+if [[ $? == 0 ]] ; 
+then
+#remove old version assets
+rm -r Superset_assets
+#rename
 find . -depth -type d -name 'assets_export*' -execdir mv {} Superset_assets \;
-
+fi ;
